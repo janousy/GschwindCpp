3205f83ba4fb1217842b96c0048884c6a72ef359
janousy <janosch.baltensperger@gmail.com>
2020-09-22 16:38:09 +0200

    working exercise2

--- LE1/Exercise1/main.cpp
+++ LE1/Exercise1/main.cpp
@@ -1,33 +1,41 @@
 #include <iostream>
-
-#include <iostream>
 #include <fstream>
 
 /* Exercise 1.1
- * Implement a program that converts Unix style text files to MSDOS
-    style text files and vice-versa
+Implement a program that converts Unix style text files to MSDOS
+style text files and vice-versa
 • Unix uses ‘\n’ as line separators
 • MSDOS uses ‘\r’ followed by ‘\n’ as line separators
-• How do you deal with incorrectly formatted MSDOS text files?
+• How do you deal with incorrectly formatted Unix text files?
  */
 
 int main(int argc, char *argv[]) {
+    //argc: number of command line arguments
     std::cout << "Hello, World!" << std::endl;
+
+    //std::cout << "input file: " << argv[1] << std::endl;
+    //std::cout << "input file: " << argv[2] << std::endl;
 
     std::ifstream ifs(argv[1]);
     std::ofstream ofs(argv[2]);
     char c;
-
-    int length = ifs.tellg();
-    std::cout << "Reading file of length: " << length << std::endl;
+    bool stream_ended = false;
 
     for (;;) {
         if (!ifs.good()) {
+            std::cout << "stream ended" << std::endl;
+            stream_ended = true;
             break;
         }
         ifs.get(c);
-        if (c == '\n') ofs << "\r\n"; else ofs << c;
+        //std::cout << c << std::endl;
+        if (c == '\n') {
+            std::cout << "converting line break" << std::endl;
+            ofs << "\r\n";
+        }
+        else {
+            if(!stream_ended) ofs << c;
+        }
     }
-
     return 0;
 }
---
+++ LE1/exercise2/main.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class fraction {
+    int cntr;
+    int denom;
+
+public:
+    //constructor
+    fraction(int c = 0, int d = 1) : cntr(c), denom(d) { /*void*/ }
+
+    int get_counter() {
+        return cntr;
+    }
+
+    void set_counter(int c) {
+        cntr = c;
+    }
+
+    int get_denominator() {
+        return denom;
+    }
+
+    void set_denominator(int d) {
+        denom = d;
+    }
+};
+
+fraction operator*(fraction a, fraction b) {
+    int f1 = __gcd(a.get_counter(), b.get_denominator());
+    int f2 = __gcd(b.get_counter(), a.get_denominator());
+    return fraction((a.get_counter() / f1) * (b.get_counter() / f2),
+                    (a.get_denominator() / f2) * (b.get_denominator() / f1));
+}
+
+fraction operator/(fraction a, fraction b) {
+    return a * fraction(b.get_denominator(), b.get_counter());
+}
+
+fraction operator+(fraction a, fraction b) {
+    int denom = (a.get_denominator() * b.get_denominator()) / __gcd(a.get_denominator(), b.get_denominator());
+    int cntr1 = denom / a.get_denominator() * a.get_counter();
+    int cntr2 = denom / b.get_denominator() * b.get_counter();
+
+    return fraction(cntr1 + cntr2, denom);
+
+}
+
+fraction operator-(fraction a, fraction b) {
+    int denom = (a.get_denominator() * b.get_denominator()) / __gcd(a.get_denominator(), b.get_denominator());
+    int cntr1 = denom / a.get_denominator() * a.get_counter();
+    int cntr2 = denom / b.get_denominator() * b.get_counter();
+
+    return fraction(cntr1 - cntr2, denom);
+
+}
+
+ostream &operator<<(ostream &os, fraction f) {
+    os << '(' << f.get_counter() << '/' << f.get_denominator() << ')';
+    return os;
+}
+
+void check_char(char ch, istream &is) {
+    char c;
+    is >> c;
+    if (c != ch) {
+        is.putback(c);
+        is.setstate(ios::badbit);
+    }
+}
+
+istream &operator>>(istream &is, fraction &f) {
+    fraction g;
+    int counter, denom;
+
+    check_char('(', is);
+    is >> counter;
+    check_char('/', is);
+    is >> denom;
+    check_char(')', is);
+    g.set_counter(counter);
+    g.set_denominator(denom);
+
+    if (is) f = g;
+
+    return is;
+}
+
+int main() {
+    fraction f1(2, 3);
+    fraction f2(1, 4);
+
+    cout << "f1: " << f1 << std::endl;
+    cout << "f2: " << f2 << std::endl;
+
+    fraction res1 = f1 * f2;
+    cout << "f1 * f2 = " << res1 << endl;
+
+    fraction input1;
+    fraction input2;
+
+    cout << "Your first fraction: " << endl;
+    cin >> input1;
+    cout << input1 << endl;
+    cout << "Your second fraction: " << endl;
+    cin >> input2;
+    cout << input2 << endl;
+
+    cout << "operation (+,-,*,/) ?" << endl;
+    string op;
+    cin >> op;
+
+    if (op == "*") {
+        cout << input1 * input2;
+    } else if (op == "/") {
+        cout << input1 / input2;
+    } else if (op == "-") {
+        cout << input1 - input2;
+    } else if (op == "+") {
+        cout << input1 + input2;
+    } else {
+        cout << "invalid operation";
+    }
+    return 0;
+}
