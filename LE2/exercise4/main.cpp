364042a34e0f3e354fc69932ef679955159707fd
Janosch Baltensperger <janosch.baltensperger@uzh.ch>
2020-10-13 16:09:07 +0200

    ex2 solved

--- LE2/exercise4/main.cpp
+++ LE2/exercise4/main.cpp
@@ -7,6 +7,47 @@
 
 using namespace std;
 
+int sum_direction(int hOrg, int cOrg, int hDir, int cDir,  playfield &pf){
+    int h = hOrg + hDir;
+    int c = cOrg + cDir;
+    int sum = 0;
+
+    while (h< pf.height && h>= 0 && c< pf.width && c>= 0){
+
+        if(pf.stoneat(hOrg,cOrg) == pf.stoneat(h,c)){
+            sum = sum +1;
+            h = h + hDir;
+            c = c + cDir;
+        }
+        else{
+            break;
+        }
+    }
+
+    return sum;
+}
+
+bool game_finished(int hOrg, int cOrg, playfield &pf) {
+
+    int h = hOrg;
+    int c = cOrg;
+    //check vertical
+    if (sum_direction(h, c, 1, 0, pf) >= 3) {
+        return true;
+    }
+        //check to top left and buttom right
+    else if (sum_direction(h, c, -1, -1, pf) + sum_direction(h, c, 1, 1, pf) >= 3) {
+        return true;
+    }
+        //check to top right and buttom left
+    else if (sum_direction(h, c, -1, 1, pf) + sum_direction(h, c, 1, -1, pf) >= 3) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
 void print_playfield(playfield pf) {
     for (int h = 0; h < pf.height; h++) {
         for (int w = 0; w < pf.width; w++) {
@@ -16,16 +57,13 @@ void print_playfield(playfield pf) {
     }
 }
 
-void insert_stone(playfield &pf, int w) {
+int insert_stone(playfield &pf, int c, int playerSign) {
     int h = 0;
-    while (pf.stoneat(h+1, w) == 0 && h < playfield::height - 1) {
+    while (pf.stoneat(h + 1, c) == 0 && h + 1 < playfield::height) {
         h++;
     }
-    pf.rep[h][w] = 1;
-}
-
-bool check_game_ended(playfield &pf) {
-    return false;
+    pf.rep[h][c] = playerSign;
+    return h;
 }
 
 int main() {
@@ -38,8 +76,6 @@ int main() {
 
     playfield pf;
 
-    bool game_ended = false;
-
     for (int h = 0; h < pf.height; h++) {
         for (int w = 0; w < pf.width; w++) {
             pf.rep[h][w] = pf.none;
@@ -47,14 +83,18 @@ int main() {
     }
 
     print_playfield(pf);
-    while (!game_ended) {
-        for (human<playfield> p : players) {
-            int input = p.play(pf);
-            insert_stone(pf, input);
-            print_playfield(pf);
-            game_ended = check_game_ended(pf);
+    int turn = 0;
+    for (;;) {
+        int c, h;
+        c = h1.play(pf);
+        h = insert_stone(pf, c, turn + 1);
+        print_playfield(pf);
+        if (game_finished(h, c, pf)) {
+            cout << "Game Finished player" << turn + 1 << "won" << endl;
+            break;
         }
-    }
 
+        turn = (turn + 1) % 2;
+    }
     return 0;
 }
