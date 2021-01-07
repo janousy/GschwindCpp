e5cc158cbe0a71f41aeec951dba3a3599a2cebc9
Janosch Baltensperger <janosch.baltensperger@uzh.ch>
2021-01-07 11:36:54 +0100

    add #10 shapeFactory

---
+++ exam/exercises/10_Factory/cmake-build-debug/CMakeFiles/10_Factory.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "10_Factory"
+  "10_Factory.pdb"
+  "CMakeFiles/10_Factory.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/10_Factory.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- exam/exercises/3_pVector_PersistentTraits/cmake-build-debug/cmake_install.cmake
+++ exam/exercises/3_pVector_PersistentTraits/cmake-build-debug/cmake_install.cmake
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/janoschbaltensperger/repos/GeschwindCpp/exam/exercises/2_pVector_PersistentTraits/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/janoschbaltensperger/repos/GeschwindCpp/exam/exercises/3_pVector_PersistentTraits/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
